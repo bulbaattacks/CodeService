de8f6b97d6b8e96958d82bf2d0da0592df8702e8
Arina Zubova <arina777pototskaya@gmail.com>
2026-01-03 18:19:04 +0300

    init commit

---
+++ src/main/java/io/github/bulbaattacks/CodeService/CodeServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.bulbaattacks.CodeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeServiceApplication.class, args);
+	}
+
+}
